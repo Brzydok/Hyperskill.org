51400395e1022591c608ad63b62c1d5782568e0e
Brzydok <67192887+Brzydok@users.noreply.github.com>
2023-11-03 11:10:52 +0100

    Create stage_1.py

---
+++ Python/Web scrapper/stage_1.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+url = input()
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    if 'content' in data:
+        print(data['content'])
+    else:
+        print("Invalid quote resource!")
+else:
+        print("Invalid quote resource!")
